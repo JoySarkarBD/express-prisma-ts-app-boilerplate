83b0766e0dffcccb49bcd52a614c4bc84938b3d5
JoySarkarBD <developer.joysarkar@gmail.com>
2024-09-19 21:25:53 +0600

    chore: instead of bcrypt now it will use bcryptjs

--- src/utils/bcrypt/compare-info.ts
+++ src/utils/bcrypt/compare-info.ts
@@ -8,7 +8,7 @@ import bcrypt from 'bcryptjs';
  * @returns {Promise<boolean>} - A promise that resolves to a boolean indicating if the data matches the hash.
  */
 const compareInfo = async (data: string, hash: string): Promise<boolean> => {
-  // Compare the plain data with the hashed data using bcryptjs
+  // Compare the plain data with the hashed data using bcrypt
   return await bcrypt.compare(data, hash);
 };
 
