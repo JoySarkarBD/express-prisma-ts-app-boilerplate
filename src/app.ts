664a4ef6750a72a6883952dba56900d6fecc6605
JoySarkarBD <developer.joysarkar@gmail.com>
2024-12-30 10:16:54 +0600

    chore: application run time reduced

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,16 @@
-// External imports
+import express, { Application, Request, Response } from 'express';
+import path from 'path';
+import config from './config/config';
+
+// Security and Middleware imports
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-import express, { Application, Request, Response } from 'express';
 import fileUpload from 'express-fileupload';
-import fs from 'fs';
-import path from 'path';
-
-// Security Middleware Import
 import mongoSanitize from 'express-mongo-sanitize';
 import rateLimit from 'express-rate-limit';
 import helmet from 'helmet';
 import hpp from 'hpp';
 import morgan from 'morgan';
-import config from './config/config';
-import PathNotFound from './helpers/responses/path-not-found';
-import { loggerStream } from './utils/logger/logger';
 
 // Express app initialization
 const app: Application = express();
@@ -35,67 +31,56 @@ app.use(mongoSanitize());
 app.use(hpp());
 app.use(morgan('dev'));
 
-// Use Morgan with the custom logger
-app.use(morgan('combined', { stream: loggerStream }));
-
 // Request Rate Limiting
-const limiter = rateLimit({
-  windowMs: config.REQUEST_LIMIT_TIME,
-  max: config.NODE_ENV !== 'production' ? Infinity : config.REQUEST_LIMIT_NUMBER, // unlimited in any mode except production
-  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-});
-
-app.use(limiter);
+app.use(
+  rateLimit({
+    windowMs: config.REQUEST_LIMIT_TIME,
+    max: config.NODE_ENV !== 'production' ? Infinity : config.REQUEST_LIMIT_NUMBER,
+    standardHeaders: true,
+    legacyHeaders: false,
+  })
+);
 
 // Serve static files from the public directory
-app.use(express.static(publicDirPath));
+app.use(
+  express.static(publicDirPath, {
+    setHeaders: (res) => {
+      res.setHeader('Cross-Origin-Resource-Policy', 'cross-origin');
+    },
+  })
+);
 
 // Recursive function to load routes from nested folders
 export const routes: { module: string; path: string; method: string; time: number }[] = [];
 
 const loadRoutes = (basePath: string, baseRoute: string) => {
+  const fs = require('fs'); // Load fs only here
   if (fs.existsSync(basePath)) {
-    fs.readdirSync(basePath).forEach((item) => {
+    fs.readdirSync(basePath).forEach((item: string) => {
       const itemPath = path.join(basePath, item);
       const routePrefix = `${baseRoute}/${item.replace('.route', '')}`;
 
-      // Measure the loading time of each route
       const start = performance.now();
-
       if (fs.statSync(itemPath).isDirectory()) {
-        // Recursively load routes for nested folders
         loadRoutes(itemPath, routePrefix);
       } else if (item.endsWith('.route.ts') || item.endsWith('.route.js')) {
-        // Dynamically load the route file
-        app.use(baseRoute, require(itemPath));
-
-        // Dynamically load the route file
         const routeModule = require(itemPath);
+        app.use(baseRoute, routeModule);
 
-        // Log routes in any mode except production
         if (config.NODE_ENV !== 'production') {
           const end = performance.now();
-          const loadTime = end - start;
-
-          if (routeModule) {
-            // Capture route paths and methods
-            routeModule.stack.forEach((layer: any) => {
-              if (layer.route) {
-                const methods = Object.keys(layer.route.methods).map((method) =>
-                  method.toUpperCase()
-                );
-                methods.forEach((method) => {
-                  routes.push({
-                    module: item.split('.')[0],
-                    path: `${baseRoute}${layer.route.path}`,
-                    method,
-                    time: loadTime,
-                  });
+          routeModule.stack.forEach((layer: any) => {
+            if (layer.route) {
+              Object.keys(layer.route.methods).forEach((method) => {
+                routes.push({
+                  module: item.split('.')[0],
+                  path: `${baseRoute}${layer.route.path}`,
+                  method: method.toUpperCase(),
+                  time: end - start,
                 });
-              }
-            });
-          }
+              });
+            }
+          });
         }
       }
     });
@@ -108,11 +93,7 @@ loadRoutes(routesPath, '/api/v1');
 
 // Serve an image file on the root route
 app.get('/', (req: Request, res: Response) => {
-  // Path to the image file
-  const imagePath = path.join(publicDirPath, 'images', 'index.png');
-
-  // Send the image file as a response
-  res.sendFile(imagePath, (err) => {
+  res.sendFile(path.join(publicDirPath, 'images', 'index.png'), (err) => {
     if (err) {
       console.error(`Failed to send image file: ${err.message}`);
       res.status(500).send('Failed to send image.');
@@ -120,8 +101,8 @@ app.get('/', (req: Request, res: Response) => {
   });
 });
 
-// Use the "Path not found" handler after all routes
+// Path not found handler
+import PathNotFound from './helpers/responses/path-not-found';
 app.use(PathNotFound);
 
-// Module exports
 export default app;
