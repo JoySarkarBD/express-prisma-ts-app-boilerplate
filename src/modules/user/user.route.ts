b431b3fa9b3e53e9bdd5ca3f4ade9e03f2ab2be0
JOY_SARKAR <77662668+JoySarkarBD@users.noreply.github.com>
2025-02-20 23:27:43 +0600

    Update user.route.ts

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -40,23 +40,23 @@ router.post("/create-user", validateCreateUser, createUser);
 router.post("/create-user/many", validateCreateManyUser, createManyUser);
 
 /**
- * @route PUT /api/v1/user/update-user/many
+ * @route PATCH /api/v1/user/update-user/many
  * @description Update multiple users information
  * @access Public
  * @param {function} controller - ['updateManyUser']
  * @param {function} validation - ['validateIds', 'validateUpdateManyUser']
  */
-router.put("/update-user/many", validateIds, validateUpdateManyUser, updateManyUser);
+router.patch("/update-user/many", validateIds, validateUpdateManyUser, updateManyUser);
 
 /**
- * @route PUT /api/v1/user/update-user/:id
+ * @route PATCH /api/v1/user/update-user/:id
  * @description Update user information
  * @param {string} id - The ID of the user to update
  * @access Public
  * @param {function} controller - ['updateUser']
  * @param {function} validation - ['validateId', 'validateUpdateUser']
  */
-router.put("/update-user/:id", validateId, validateUpdateUser, updateUser);
+router.patch("/update-user/:id", validateId, validateUpdateUser, updateUser);
 
 /**
  * @route DELETE /api/v1/user/delete-user/many
